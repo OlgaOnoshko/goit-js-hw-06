ad97387dad55af9781fc2eef05c3c2ad5b241b2d
OlgaOnoshko <onoshko.olga@gmail.com>
2021-10-18 23:55:27 +0300

    Task1 update

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,8 @@ const categoryItem = document.querySelectorAll("li.item")
 console.log(`Number of categories: ${categoryItem.length}`)
 
 categoryItem.forEach(item => {
-    const categoryName = item.querySelector("h2").innerText
-    const categoryNumber = item.querySelectorAll("li").length
+    const categoryName = item.firstElementChild.innerText
+    const categoryNumber = item.lastElementChild.children.length
     console.log(`Category: ${categoryName}`)
     console.log(`Elements: ${categoryNumber}`)
 })
